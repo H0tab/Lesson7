53a690d37fca5933aa590549ac2221136c9f78d4
Александр Князев <ksasha18@mail.ru>
2020-10-10 22:45:57 +0300

    Добавьте файлы проекта.

---
+++ Lesson7/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lesson7.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lesson7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        IEnumerable<Company> companies = new List<Company>
+        {
+            new Company{Id = 1, Name = "Apple"},
+            new Company{Id = 2, Name = "Samsung"},
+            new Company{Id = 3, Name = "Google"}
+        };
+
+        public IActionResult Create()
+        {
+            ViewBag.Companies = new SelectList(companies, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public string Create(Product product)
+        {
+            Company company = companies.FirstOrDefault(c => c.Id == product.CompanyId);
+            return $"Добавлен новый элемент: {product.Name} ({company?.Name})";
+        }
+    }
+}
---
+++ Lesson7/TagHelpers/TimerTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson7.TagHelpers
+{
+    public class TimerTagHelper : TagHelper
+    {
+        public List<string> Lines { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            string listContent = "";
+            foreach (string item in Lines)
+                listContent += "<li>" + item + "</li>";
+
+            output.Content.SetHtmlContent(listContent);
+        }
+    }
+}
